29559d435ea30dad9b2c5148eb1aafdd32d32ef1
kracenaz <krisharace.naz@edu.sait.ca>
2026-06-06 23:04:11 -0600

    Add more details (image for button row and fixed some sizing)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,63 +1,53 @@
-import {
-  Button,
-  Image,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import {Button, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View,} from "react-native";
 
-const albumCover =
-  "https://images.genius.com/c6619186e8f93ff5c7ce61912e08d9c0.1000x1000x1.png";
-
-const artistPhoto =
-  "https://i.scdn.co/image/ab6761610000e5eb4293385d324db8558179afd9";
-
-const tracks = [
-  "Make Them Cry",
-  "Dust",
-  "Whisper My Name",
-  "Janice STFU",
-  "Ran To Atlanta",
-  "Make Them Pay",
-];
+const albumCover = "https://images.genius.com/c6619186e8f93ff5c7ce61912e08d9c0.1000x1000x1.png";
+const artistPhoto = "https://i.scdn.co/image/ab6761610000e5eb4293385d324db8558179afd9";
+const tracks = ["Make Them Cry", "Dust", "Whisper My Name", "Janice STFU", "Ran To Atlanta",];
+const Tower = "https://www.cp24.com/resizer/v2/F2QOJ5FXDBCAZNGN7SPIFRJRA4.jpg?smart=true&auth=55d4391422192688a093d2be568b6c519bca36f2d0b9587e3bd6864d756b458b&width=1200&height=630";
 
 export default function Index() {
   return (
+    // Album Cover, Album Title and Info and Artist Name
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.content}>
         <Text style={styles.backButton}>‹</Text>
-
         <Image source={{ uri: albumCover }} style={styles.albumImage} />
-
         <Text style={styles.title}>ICEMAN</Text>
-
         <View style={styles.artistRow}>
           <Image source={{ uri: artistPhoto }} style={styles.artistImage} />
           <Text style={styles.artistName}>Drake</Text>
         </View>
-
         <Text style={styles.albumInfo}>
           Album • May 15 • Latest release
         </Text>
 
-        {/* Spotify Buttons */}
+        {/* Row after Album and Artist Info - Music Buttons */}
         <View style={styles.controlRow}>
+          <Image source={{ uri: Tower }} style={styles.towerSize} />
           <Text style={styles.icon}>＋</Text>
-          <Text style={styles.icon}>↓</Text>
+          <Text style={styles.icon}> ↓</Text>
           <Text style={styles.icon}>•••</Text>
-
           <View style={{ flex: 1 }} />
 
           <Text style={styles.icon}>⇄</Text>
-
           <TouchableOpacity style={styles.playButton}>
             <Text style={styles.playText}>▶</Text>
           </TouchableOpacity>
         </View>
 
-        {/* Alert Button */}
+        {/* //Track List with Artist Name and additional inserts*/}
+        {tracks.map((track, index) => (
+          <View key={index} style={styles.trackRow}>
+            <View style={styles.trackTextBox}>
+              <Text style={styles.trackTitle}>{track}</Text>
+              <Text style={styles.trackInfo}>E ⏵︎ Video • Drake</Text>
+            </View>
+            <Text style={styles.dotsBesideTracks}>• • •</Text>
+          </View>
+        ))}
+      </ScrollView>
+
+      {/* Working Alert Button - Press to activate */}
         <View style={styles.alertContainer}>
           <Button
             title="Alert"
@@ -66,18 +56,6 @@ export default function Index() {
             }}
           />
         </View>
-
-        {tracks.map((track, index) => (
-          <View key={index} style={styles.trackRow}>
-            <View style={styles.trackTextBox}>
-              <Text style={styles.trackTitle}>{track}</Text>
-              <Text style={styles.trackInfo}>E Video • Drake</Text>
-            </View>
-
-            <Text style={styles.trackMenu}>•••</Text>
-          </View>
-        ))}
-      </ScrollView>
     </View>
   );
 }
@@ -86,6 +64,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#111111",
+  },
+
+  towerSize: {
+    width: 33,
+    height: 40,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#aaaaaa",
+    marginRight: 20,
   },
 
   content: {
@@ -149,6 +136,7 @@ const styles = StyleSheet.create({
     color: "#aaaaaa",
     fontSize: 28,
     marginRight: 24,
+    borderColor: "#aaaaaa",
   },
 
   playButton: {
@@ -192,8 +180,8 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
 
-  trackMenu: {
+  dotsBesideTracks: {
     color: "#aaaaaa",
-    fontSize: 22,
+    fontSize: 15,
   },
 });
